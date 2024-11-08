f016040fde32b0a484760fce38c0dc3d2b027637
JIJ390 <wwdi1235@gmail.com>
2024-11-08 17:49:44 +0900

    111

---
+++ todo-front/src/components/TodoList.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useContext } from 'react';
+
+const TodoListComponent = () => {
+
+  return(
+    <>
+      <h1>○○○ 의 TodoList</h1>
+    </>
+  )
+}
+
+export default TodoListComponent
